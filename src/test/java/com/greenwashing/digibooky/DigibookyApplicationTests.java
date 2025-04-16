6b4559dd17f61151ebb98d9a2eed4452b76e2238
Andrea Basili <andrea.basili@live.it>
2025-04-16 18:34:29 +0200

    initial setup

---
+++ src/test/java/com/greenwashing/digibooky/DigibookyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenwashing.digibooky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigibookyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
