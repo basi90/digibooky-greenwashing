b68df62aed692c48b29b15c023ffc1442f609f4a
Andrea Basili <andrea.basili@live.it>
2025-04-17 16:03:45 +0200

    bookDTO tests

--- src/test/java/com/greenwashing/digibooky/service/DTOs/BookDTOTest.java
+++ src/test/java/com/greenwashing/digibooky/service/DTOs/BookDTOTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 
@@ -22,7 +23,36 @@ public class BookDTOTest {
         BookInputDTO inputDTO = new BookInputDTO("a", 1, "b", "c");
 
         String jsonString = """
-                {}
+                {
+                "title":"a",
+                "authorId":1,
+                "description":"b",
+                "isbn":"c"
+                }
                 """;
+
+        assertThat(inputJson.write(inputDTO)).isEqualTo(jsonString);
+    }
+
+    @Test
+    void testSerializeOutputDTO() throws IOException {
+        AuthorOutputDTO authorDTO = new AuthorOutputDTO(1, "a", "b");
+        BookOutputDTO outputDTO = new BookOutputDTO(1, "a", authorDTO, "b", "c");
+
+        String jsonString = """
+                {
+                "id":1,
+                "title":"a",
+                "author":{
+                    "id":1,
+                    "firstName":"a",
+                    "lastName":"b"
+                    },
+                "description":"b",
+                "isbn":"c"
+                }
+                """;
+
+        assertThat(outputJson.write(outputDTO)).isEqualTo(jsonString);
     }
 }
