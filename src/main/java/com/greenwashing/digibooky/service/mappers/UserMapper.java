4252e8279943367ba9cc5df69f8e54cc29db0af7
nicolas bluj <nicolas_bluj@acslworld.com>
2025-04-17 13:44:45 +0200

    Rental mapper and usermapper done

--- src/main/java/com/greenwashing/digibooky/service/mappers/UserMapper.java
+++ src/main/java/com/greenwashing/digibooky/service/mappers/UserMapper.java
@@ -1,4 +1,32 @@
 package com.greenwashing.digibooky.service.mappers;
 
+import com.greenwashing.digibooky.domain.User;
+import com.greenwashing.digibooky.service.DTOs.UserInputDTO;
+import com.greenwashing.digibooky.service.DTOs.UserOutputDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+    private UserMapper() {}
+
+    public User inputDTOtoUser(UserInputDTO userInputDTO) {
+        return new User(userInputDTO.getRole(),
+                userInputDTO.getSsn(),
+                userInputDTO.getEmail(),
+                userInputDTO.getLastName(),
+                userInputDTO.getCity());
+    }
+
+    public UserOutputDTO userToOutputDTO(User user) {
+        return new UserOutputDTO(user.getId(),
+                user.getRole(),
+                user.getSsn(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getStreetName(),
+                user.getStreetNumber(),
+                user.getCity(),
+                user.getPostalCode());
+    }
 }
