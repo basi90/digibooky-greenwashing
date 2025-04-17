1566908a993b293db75f6bd4c8382de9a453aa30
q-bckrt <pom.schc@egmail.com>
2025-04-17 11:31:09 +0200

    done basic author repository

--- src/main/java/com/greenwashing/digibooky/infrastructure/AuthorRepository.java
+++ src/main/java/com/greenwashing/digibooky/infrastructure/AuthorRepository.java
@@ -1,4 +1,37 @@
 package com.greenwashing.digibooky.infrastructure;
 
+import com.greenwashing.digibooky.domain.Author;
+import com.greenwashing.digibooky.service.DTOs.AuthorOutputDTO;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
 public class AuthorRepository {
+
+    // FIELDS
+    HashMap<Long, Author> authors;
+
+    // CONSTRUCTOR
+    public AuthorRepository() {
+        authors = new HashMap<>();
+    }
+
+    // METHODS
+    public void save(Author author) {
+        authors.put(author.getId(), author);
+    }
+
+    public void delete(long id) {
+        authors.remove(id);
+    }
+
+    public Author getById(long id) {
+        return authors.get(id);
+    }
+
+    public Collection<Author> getAll() {
+        return authors.values();
+    }
 }
