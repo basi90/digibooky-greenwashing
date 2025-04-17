e14fbdb138c837b4dd3c577a6447b20c406615b2
Andrea Basili <andrea.basili@live.it>
2025-04-17 16:17:28 +0200

    userDTO tests

---
+++ src/test/java/com/greenwashing/digibooky/service/DTOs/UserDTOTest.java
@@ -0,0 +1,62 @@
+package com.greenwashing.digibooky.service.DTOs;
+
+import com.greenwashing.digibooky.domain.UserRole;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
+@JsonTest
+public class UserDTOTest {
+
+    @Autowired
+    private JacksonTester<UserInputDTO> inputJson;
+
+    @Autowired
+    private JacksonTester<UserOutputDTO> outputJson;
+
+    @Test
+    void testSerializeInputDTO() throws IOException {
+        UserInputDTO inputDTO = new UserInputDTO(UserRole.MEMBER, "a", "x@y.z", "c", "d", "e", 1, "Brussels", "f");
+
+        String jsonString = """
+                {
+                "role":"MEMBER",
+                "ssn":"a",
+                "email":"x@y.z",
+                "firstName":"c",
+                "lastName":"d",
+                "streetName":"e",
+                "streetNumber":1,
+                "city":"Brussels",
+                "postalCode":"f"
+                }
+                """;
+
+        assertThat(inputJson.write(inputDTO)).isEqualTo(jsonString);
+    }
+
+    @Test
+    void testSerializeOutputDTO() throws IOException {
+        UserOutputDTO outputDTO = new UserOutputDTO(1,UserRole.MEMBER, "a", "x@y.z", "c", "d", "e", 1, "Brussels", "f");
+
+        String jsonString = """
+                {
+                "role":"MEMBER",
+                "ssn":"a",
+                "email":"x@y.z",
+                "firstName":"c",
+                "lastName":"d",
+                "streetName":"e",
+                "streetNumber":1,
+                "city":"Brussels",
+                "postalCode":"f"
+                }
+                """;
+
+        assertThat(outputJson.write(outputDTO)).isEqualTo(jsonString);
+    }
+}
